199cdbcb448daf73b7b9b25afbee28a2fb1ebb18
weizi0328 <ankewang26@gmail.com>
2022-09-06 22:11:37 +0800

    feat: add isAuthenticated to display different view templates

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,8 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 
+const User = require('../models/user')
+
 module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,16 +1,21 @@
 const express = require('express')
 const router = express.Router()
-const User = require('../../models/user')
+
 const passport = require('passport')
 
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.get('/logout', (req, res) => {
-  req.logout()
-  res.redirect('/users/login')
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: 'users/login'
+}))
+
+router.get('/register', (req, res) => {
+  res.render('register')
 })
 
 router.post('/register', (req, res) => {
@@ -38,8 +43,10 @@ router.post('/register', (req, res) => {
     .catch(err => console.log(err))
 })
 
-router.get('/register', (req, res) => {
-  res.render('register')
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
 })
+
 
 module.exports = router
